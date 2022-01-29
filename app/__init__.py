6b727763c31082945c3bb674f7faf724ef40c17a
hep <ephong93@gmail.com>
2022-01-29 12:35:26 +0900

    :fire: cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-from flask_cors import CORS
 from flask_migrate import Migrate
 
 migrate = Migrate()
 socketio = SocketIO()
 db = SQLAlchemy()
-cors = CORS()
 
 def create_app():
     app = Flask(__name__, static_folder='../build', static_url_path='/')
     app.config['SECRET_KEY'] = 'secret!'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['CORS_HEADERS'] = 'Content-Type'
-    cors.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
     socketio.init_app(app, cors_allowed_origins='*')
