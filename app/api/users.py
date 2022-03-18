70d4516306cae291e940d051d23effe4ee8404ec
ephong93 <ephong93@gmail.com>
2022-03-18 15:48:29 +0900

    User response includes token

--- app/api/users.py
+++ app/api/users.py
@@ -1,4 +1,5 @@
 from app.api import bp
+from app.errors import error_response
 from app.models import User
 from app import db
 from flask import request
@@ -13,6 +14,29 @@ def enter():
     if user.username is None:
         user.username = f'Guest_{user.id}'
         db.session.commit()
+
+    return {
+        'user': user.to_dict(include_token=True)
+    }
+
+
+@bp.route('/users/me', methods=['GET'])
+def get_me():
+    def get_token():
+        auth = request.headers.get('Authorization')
+        if auth and 'Bearer' in auth:
+            token = auth.split('Bearer ')[1]
+            return token
+        return None
+
+    token = get_token()
+
+    if token is None:
+        return error_response(401)
+
+    user = User.check_token(token)
+    if user is None:
+        return error_response(401)
 
     return {
         'user': user.to_dict(include_token=True)
